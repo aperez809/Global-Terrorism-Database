69e42806ed57f6a1e6d7c4bc3c11e0042c23fbb2
Hyeonjung Ko <hjko1203@gmail.com>
2018-08-15 10:43:26 -0400

    eung

--- src/main/TerrorismMySQL.java
+++ src/main/TerrorismMySQL.java
@@ -9,4 +9,85 @@ import java.util.*;
 public class TerrorismMySQL implements TerrorismAPI {
 
   // need a constructor
+
+  @Override
+  public Event getEventById(int eventId) {
+    return null;
+  }
+
+  @Override
+  public int insertEvent(Event event) {
+    return 0;
+  }
+
+  @Override
+  public int getOrInsertAttackType(AttackType attackType) {
+    return 0;
+  }
+
+  @Override
+  public int getOrInsertCity(City city) {
+    return 0;
+  }
+
+  @Override
+  public int getOrInsertCountry(Country country) {
+    return 0;
+  }
+
+  @Override
+  public int insertEventTarget(EventTarget eventTarget) {
+    return 0;
+  }
+
+  @Override
+  public int insertLocation(Location location) {
+    return 0;
+  }
+
+  @Override
+  public int getOrInsertNationality(Nationality nationality) {
+    return 0;
+  }
+
+  @Override
+  public int insertOutcome(Outcome outcome) {
+    return 0;
+  }
+
+  @Override
+  public int getOrInsertProvstate(Provstate provstate) {
+    return 0;
+  }
+
+  @Override
+  public int getOrInsertRegion(Region region) {
+    return 0;
+  }
+
+  @Override
+  public int getOrInsertTarget(Target target) {
+    return 0;
+  }
+
+  @Override
+  public int getOrInsertTargetType(TargetType targetType) {
+    return 0;
+  }
+
+  @Override
+  public int getOrInsertWeaponType(WeaponType weaponType) {
+    return 0;
+  }
+
+  @Override
+  public void setConnection(String url, String user, String password) {
+
+  }
+
+  @Override
+  public void closeConnection() {
+
+  }
+
 }
